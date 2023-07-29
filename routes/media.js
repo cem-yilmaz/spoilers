ae685fa20cea589618a90101b2b6cafad150ce88
Cem Yilmaz <cemyilmaz863@gmail.com>
2023-07-29 17:05:33 +0100

    still working on getting parts to actually work at all

--- routes/media.js
+++ routes/media.js
@@ -14,12 +14,35 @@ router.get('/new', (req, res) => {
 router.get('/:id/edit', async (req, res) => {
   try {
     const mediaItem = await Media.findById(req.params.id);
-    res.render('media/edit', { media: mediaItem, title: `Edit ${mediaItem.title}` });
+    let partDesignator = 'None';
+    let numParts = 0;
+    let customDesignator = '';
+    if(mediaItem.parts.length > 0) {
+      numParts = mediaItem.parts.length;
+      let prefix = mediaItem.parts[0].title.split(' ')[0];
+      if(['Chapter', 'Episode', 'Level', 'Part'].includes(prefix)) {
+        partDesignator = prefix;
+      } else {
+        let allStartWithSamePrefix = mediaItem.parts.every(part => part.title.startsWith(prefix));
+        if(allStartWithSamePrefix) {
+          partDesignator = 'Custom';
+          customDesignator = prefix;
+        }
+      }
+    }
+    res.render('media/edit', { 
+      media: mediaItem, 
+      title: `Edit ${mediaItem.title}`, 
+      partDesignator,
+      numParts,
+      customDesignator 
+    });
   } catch (err) {
     console.log(err);
     res.redirect('/media');
   }
 });
+
 
 router.get('/:id', async (req, res) => {
   try {
@@ -33,7 +56,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const parts = req.body.parts.map(title => ({ title })); // Map the parts titles to parts objects
+  console.log(req.body);
+  const parts = req.body.parts ? req.body.parts.map(title => ({ title })) : []; // Map the parts titles to parts objects
   const newMedia = new Media({
     title: req.body.title,
     type: req.body.type,
@@ -59,8 +83,9 @@ router.delete('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
+    console.log(req.body);
     let mediaItem = await Media.findById(req.params.id);
-    const parts = req.body.parts.map(title => ({ title })); // Map the parts titles to parts objects
+    const parts = req.body.parts ? req.body.parts.map(title => ({ title })) : []; // Map the parts titles to parts objects
     mediaItem.title = req.body.title;
     mediaItem.type = req.body.type;
     mediaItem.parts = parts;
