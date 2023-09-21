b1e32b270a0ce54c7d731eac04661e158fe286ac
cem-yilmaz <cemyilmaz863@gmail.com>
2023-09-21 13:04:29 +0100

    Prepping for actual DOM Manipulation
    also changed body to always be wide

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,35 +1,55 @@
-console.log('content-script.js locked and loaded!'); //DEBUG
+console.log('content-script.js initialized!');
 
 function checkForSpoilers() {
-    const videoLinks = document.querySelectorAll("a.ytd-thumbnail");
-  
-    // Get global filtered URLs from background.js
-    chrome.runtime.sendMessage({ action: 'getFilteredUrls' }, (filteredUrls) => {
-      videoLinks.forEach((linkElement) => {
-        const videoUrl = linkElement.href;
-    
-        if (filteredUrls.includes(videoUrl)) {
-          const titleElement = linkElement.querySelector('#video-title');
-    
-          // Update the title to "SpoilerBlocked"
-          if (titleElement) {
-            titleElement.textContent = "SpoilerBlocked";
-          }
+    // Array to store all video data
+    let videoDataArray = [];
+
+    // Iterate through all videos on the page
+    const videoLinks = document.querySelectorAll("[data-context-item-id]");
+
+    videoLinks.forEach((linkElement) => {
+        let videoData = {};
+
+        // Extract URL
+        const videoId = linkElement.getAttribute("data-context-item-id");
+        videoData["VIDEO_URL"] = `https://youtube.com/watch?v=${videoId}`;
+
+        // Extract Title
+        const titleElement = linkElement.querySelector("#video-title");
+        if (titleElement) {
+            videoData["VIDEO_TITLE"] = titleElement.textContent.trim();
         }
-      });
+
+        // Extract Thumbnail SRC
+        const thumbnailElement = linkElement.querySelector('img');
+        if (thumbnailElement) {
+            videoData["VIDEO_THUMBNAIL_SRC"] = thumbnailElement.src;
+        }
+
+        videoDataArray.push(videoData);
+
+        // Implement other manipulations here (like changing the title and blurring the thumbnail)
+        if (titleElement) {
+            titleElement.textContent = "SponsorBlocked";
+        }
+
+        if (thumbnailElement) {
+            thumbnailElement.style.filter = 'blur(5px)';
+        }
     });
-  }
-  
-  // Run the check when the script is injected
-  checkForSpoilers();
-  
-  // Run the check on dynamic page updates
-  const targetNode = document.getElementById('content');
-  const config = { childList: true, subtree: true };
-  
-  const observer = new MutationObserver(() => {
+
+    console.log(videoDataArray);
+}
+
+// Run the check when the script is injected
+checkForSpoilers();
+
+// To handle YouTube's dynamic content loading
+const targetNode = document.getElementById('content');
+const config = { childList: true, subtree: true };
+
+const observer = new MutationObserver(() => {
     checkForSpoilers();
-  });
-  
-  observer.observe(targetNode, config);
-  
+});
+
+observer.observe(targetNode, config);
