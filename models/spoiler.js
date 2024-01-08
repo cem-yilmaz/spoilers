109cc823dc3c616833edf1850ef0a2550950ef76
cem-yilmaz <cemyilmaz863@gmail.com>
2024-01-08 16:02:05 +0000

    Cleaned up spoiler model formatting

--- models/spoiler.js
+++ models/spoiler.js
@@ -2,33 +2,12 @@ const mongoose = require('mongoose');
 const partSchema = require('./media').partSchema;
 
 const SpoilerSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  intensity: {
-    type: String,
-    required: true,
-    enum: ['No Spoilers', 'Story Beats/Mild Spoilers', 'Major Spoilers']
-  },
-  reference: {
-    type: String,
-    required: true
-  },
-  media: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Media',
-  },
-  urls: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'URL'
-  }],
-  part: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Part',
-    default: null
-  }
+  title: { type: String, required: true },
+  intensity: { type: String, required: true, enum: ['No Spoilers', 'Story Beats/Mild Spoilers', 'Major Spoilers'] },
+  reference: { type: String, required: true },
+  media: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Media' },
+  urls: [{ type: mongoose.Schema.Types.ObjectId, ref: 'URL' }],
+  part: { type: mongoose.Schema.Types.ObjectId, ref: 'Part', default: null }
 });
 
 module.exports = mongoose.model('Spoiler', SpoilerSchema);
